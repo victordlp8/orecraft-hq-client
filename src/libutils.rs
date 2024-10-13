075f74d6f5b24d8049bf0bc33e1f9444e9f63da2
Victor De Lomas <victordlp8@gmail.com>
2024-10-13 16:10:31 +0200

    Update libutils.rs

--- src/libutils.rs
+++ src/libutils.rs
@@ -1,13 +1,17 @@
-use jni::JNIEnv;
 use jni::sys::jstring;
+use jni::JNIEnv;
 
 pub fn create_error_string(env: &mut JNIEnv) -> jstring {
-    let error_string = env.new_string("-1.0,-1.0,-1.0").expect("Couldn't create error string");
+    let error_string = env
+        .new_string("-1.0,-1.0,-1.0")
+        .expect("Couldn't create error string");
     error_string.into_raw()
 }
 
 pub fn url_trimming(url: &String) -> String {
-    let trimmed_url = url.trim_start_matches("http://").trim_start_matches("https://");
+    let trimmed_url = url
+        .trim_start_matches("http://")
+        .trim_start_matches("https://");
     trimmed_url.trim_end_matches('/').to_string()
 }
 
